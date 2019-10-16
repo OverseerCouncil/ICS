10d9483081082ee8a0a295eece170326d4decf17
OverseerCouncil <49034247+OverseerCouncil@users.noreply.github.com>
2019-10-16 17:25:47 +0800

    New homework

--- test/未命名1.cpp
+++ test/未命名1.cpp
@@ -1,13 +1,44 @@
 #include<iostream>
-#include<limits.h>
+
 using namespace std;
-int x;
+typedef unsigned float_bits;
+
+float_bits float_half(float_bits f)
+{
+	unsigned sign = f >> 31;
+  	unsigned noSign = f & 0x7FFFFFFF;
+  	unsigned exp = f >> 23 & 0xFF;
+  	unsigned frac = f & 0x7FFFFF;
+	if (exp == 0xFF)
+	{
+  		return f;
+	}
+  	int add = ((frac & 0x3) == 0x3);
+	if (exp == 0) 
+	{
+  		frac >>= 1;
+   		frac += add;
+	} 
+	else if (exp == 1) 
+	{
+    	noSign >>= 1;
+    	noSign += add;
+    	exp = noSign >> 23 & 0xFF;
+    	frac = noSign & 0x7FFFFF;
+	} 
+	else 
+	{
+   		exp -= 1;
+  	}
+	return sign << 31 | exp << 23 | frac;
+}
 
 int main()
 {
-	cin>>x;
-	x=((x<<1)+x);
-	((x&INT_MIN)&&(x=x+(1<<2)-1));
-	x=x>>2;
-	cout<<x;
+	float func;
+	float rst;
+	cin>>func;
+	rst=float_half(func);
+	cout<<rst;
+	system("pause");
 }
