e2231c12cc5194412fd38e774aaa53574b3f45b0
OverseerCouncil <49034247+OverseerCouncil@users.noreply.github.com>
2020-01-23 11:02:24 +0800

    end of this term

---
+++ homework6/3.71/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BufferSize 8
+
+int good_echo()
+{
+	char* buffer = (char*)calloc(BufferSize,sizeof(char));
+	if (buffer == NULL)
+	{
+		fprintf(stderr, "Error: failed to allocate buffer.\n");
+		return -1;
+	}
+	while (1)
+	{
+		fgets(buffer, BufferSize, stdin);
+		if (strlen(buffer) == BufferSize - 1)
+		{
+			fputs(buffer, stdout);
+			if (buffer[BufferSize - 1 - 1] == '\n')
+			{
+				break;
+			}
+			memset(buffer, 0, strlen(buffer));
+		}
+		else if (strlen(buffer) < BufferSize - 1)
+		{
+			fputs(buffer, stdout);
+			break;
+		}
+	}
+	free(buffer);
+	return 0;
+}
+int main()
+{
+	good_echo();
+}
+
